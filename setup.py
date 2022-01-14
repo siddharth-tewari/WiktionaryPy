ca5a1b1a89d8de9aae91197593d4505ec11ab06f
Siddharth Tewari <76590952+siddharth-tewari@users.noreply.github.com>
2022-01-14 15:42:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   description = 'Wiktionary Dictionary. Enter the word and get opened the Wiktionary url in your default browser.',   # Give a short description about your library
   author = 'Siddharth Tewari',                   # Type in your name
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/siddharth-tewari/WiktionaryPy/archive/v_0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/siddharth-tewari/WiktionaryPy/archive/01.tar.gz',    # I explain this later on
   keywords = ['Python Dictionary', 'Wiktionary', 'Dictionary'],   # Keywords that define your package best
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
